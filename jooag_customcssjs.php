8bbdf61703d7d39a5d04a56402cba3626a1ac203
Ufuk Avcu <ufuk@jooag.de>
2015-08-18 13:33:20 +0200

    New overhauled Version
    More flexible. Lesser redundant code!

--- jooag_customcssjs.php
+++ jooag_customcssjs.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @package 	JooAg CustomCssJs
- * @version 	3.x.0 Beta
- * @for 	Joomla 3.3+ 
+ * @version 	3.1.0
+ * @for 	Joomla 3.4+ 
  * @author 	Joomla Agentur - http://www.joomla-agentur.de
  * @copyright 	Copyright (c) 2009 - 2015 Joomla-Agentur All rights reserved.
  * @license 	GNU General Public License version 2 or later;
@@ -13,49 +13,51 @@ defined('_JEXEC') or die;
 
 class plgSystemJooag_CustomCssJs extends JPlugin {
 	
-	public function onAfterInitialise() {
+	public function onAfterInitialise()
+	{
+		$this->checkState('frontend', 'frontFilesFirst');
+		$this->checkState('backend', 'backFilesFirst');
+	}
 	
-    	$app = JFactory::getApplication();
-    	$doc = JFactory::getDocument();
+	public function onBeforeRender()
+	{
+		$this->checkState('frontend', 'frontFilesLast');
+		$this->checkState('backend', 'backFilesLast');
+	}
+	
+	public function checkState($state, $xmlName){
+		
+		$app = JFactory::getApplication();
+		
+		if($state == 'frontend' and $app->isSite())
+		{
+			$this->checkAddFileType($xmlName);
+		}
+		
+		if($state == 'backend' and $app->isAdmin())
+		{
+			$this->checkAddFileType($xmlName);
+		}
+	}
+	
+	public function checkAddFileType($xmlName)
+	{
+		$doc = JFactory::getDocument();
+					
+		if($this->params->get($xmlName))
+		{
+			foreach($this->params->get($xmlName) as $file)
+			{
+				if(pathinfo('media/customcssjs/'.$file)['extension'] == 'css')
+				{
+					$doc->addStyleSheet(JURI::root().'media/customcssjs/'.$file);
+				}
 
-    	if ($app->isAdmin()){
-			if( strpos($this->params->get('backcss'), 'css') and $this->params->get('backcssposition') == '1'){
-				$doc->addStyleSheet(JURI::root().'media/customcssjs/'.$this->params->get('backcss'));
+				if(pathinfo('media/customcssjs/'.$file)['extension'] == 'js')
+				{
+					$doc->addScript(JURI::root().'media/customcssjs/'.$file);
+				}
 			}
-			if( strpos($this->params->get('backjs'), 'js') and $this->params->get('backjsposition') == '1'){
-				$doc->addScript(JURI::root().'media/customcssjs/'.$this->params->get('backjs'));
-			}	
-        }
-		else{ 
-			if( strpos($this->params->get('frontcss'), 'css') and $this->params->get('frontcssposition') == '1'){
-				$doc->addStyleSheet('media/customcssjs/'.$this->params->get('frontcss'));
-			}		
-			if( strpos($this->params->get('frontjs'), 'js') and $this->params->get('frontjsposition') == '1'){
-				$doc->addScript('media/customcssjs/'.$this->params->get('frontjs'));
-			}	
-        }
-    }
-	
-	public function onBeforeRender() {
-	
-    	$app = JFactory::getApplication();
-    	$doc = JFactory::getDocument();
-
-    	if ($app->isAdmin()){
-			if( strpos($this->params->get('backcss'), 'css') and $this->params->get('backcssposition') == '0'){
-				$doc->addStyleSheet('media/customcssjs/'.$this->params->get('backcss'));
-			}
-			if( strpos($this->params->get('backjs'), 'js') and $this->params->get('backjsposition') == '0'){
-				$doc->addScript('media/customcssjs/'.$this->params->get('backjs'));
-			}	
-        }
-		else{ 
-			if( strpos($this->params->get('frontcss'), 'css') and $this->params->get('frontcssposition') == '0'){
-				$doc->addStyleSheet('media/customcssjs/'.$this->params->get('frontcss'));
-			}		
-			if( strpos($this->params->get('frontjs'), 'js') and $this->params->get('frontjsposition') == '0'){
-				$doc->addScript('media/customcssjs/'.$this->params->get('frontjs'));
-			}	
-        }
-    }
+		}
+	}
 }
